56a09fdbb1d7566c889cc896ac6651bea7461549
Arihant416 <arihantjain416@gmail.com>
2022-08-12 23:03:40 +0530

    Factorial of Number N

---
+++ recursion/factorial.cpp
@@ -0,0 +1,42 @@
+/*
+Author : Arihant Jain
+github:https://github.com/Arihant416
+linkedin : https://www.linkedin.com/in/arihant416
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ull unsigned ll
+#define PB push_back
+#define MP make_pair
+#define flash ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define vi vector<int>
+#define vl vector<ll>
+#define vll vector<ull>
+#define mpi map<int, int>
+#define mpl map<ll, ll>
+#define mpll map<ull, ull>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define all(a) begin(a), end(a)
+#define maxEl(a) *max_element(all(a))
+#define minEl(a) *min_element(all(a))
+#define uimap unordered_map<int, int>
+#define ulmap unordered_map<ll, ll>
+#define mppii map<pi, int>
+
+int factorial(int N)
+{
+  if (N <= 1)
+    return 1;
+  return N * factorial(N - 1);
+}
+
+int32_t main()
+{
+  flash;
+  int N;
+  cin >> N;
+  cout << factorial(N) << endl;
+  return 0;
+}
