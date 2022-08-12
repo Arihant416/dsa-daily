a58610be2bb41a1527abbc45426b1f6e3f3a6992
Arihant416 <arihantjain416@gmail.com>
2022-08-12 22:40:17 +0530

    Subset generation problem recursion - Good problem

---
+++ recursion/subset_generation_problem.cpp
@@ -0,0 +1,51 @@
+/*
+Author : Arihant Jain
+github:https://github.com/Arihant416
+linkedin : https://www.linkedin.com/in/arihant416
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ull unsigned ll
+#define PB push_back
+#define MP make_pair
+#define flash ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define vi vector<int>
+#define vl vector<ll>
+#define vll vector<ull>
+#define mpi map<int, int>
+#define mpl map<ll, ll>
+#define mpll map<ull, ull>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define all(a) begin(a), end(a)
+#define maxEl(a) *max_element(all(a))
+#define minEl(a) *min_element(all(a))
+#define uimap unordered_map<int, int>
+#define ulmap unordered_map<ll, ll>
+#define mppii map<pi, int>
+
+void subsetUtil(string &s, string x, int index = 0)
+{
+  if (index == s.length())
+  {
+    cout << "Subset -> " << x << endl;
+    return;
+  }
+  subsetUtil(s, x + s[index], index + 1);
+  subsetUtil(s, x, index + 1);
+}
+void findSubsets(string &s)
+{
+  string x = "";
+  subsetUtil(s, x);
+}
+
+int32_t main()
+{
+  flash;
+  string s;
+  cin >> s;
+  findSubsets(s);
+  return 0;
+}
