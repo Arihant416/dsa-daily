d4c069bb93e3f7af72637c5f3668e86b2dfef647
Arihant416 <arihantjain416@gmail.com>
2022-08-12 23:25:17 +0530

    NCR using Recursion

---
+++ recursion/findnCr_using_recursion.cpp
@@ -0,0 +1,42 @@
+/*
+Author : Arihant Jain
+github:https://github.com/Arihant416
+linkedin : https://www.linkedin.com/in/arihant416
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ull unsigned ll
+#define PB push_back
+#define MP make_pair
+#define flash ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define vi vector<int>
+#define vl vector<ll>
+#define vll vector<ull>
+#define mpi map<int, int>
+#define mpl map<ll, ll>
+#define mpll map<ull, ull>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define all(a) begin(a), end(a)
+#define maxEl(a) *max_element(all(a))
+#define minEl(a) *min_element(all(a))
+#define uimap unordered_map<int, int>
+#define ulmap unordered_map<ll, ll>
+#define mppii map<pi, int>
+
+int nCr(int N, int R)
+{
+  if (N == R || R == 0)
+    return 1;
+  return nCr(N - 1, R - 1) + nCr(N - 1, R);
+}
+
+int32_t main()
+{
+  flash;
+  int N, R;
+  cin >> N >> R;
+  cout << nCr(N, R);
+  return 0;
+}
