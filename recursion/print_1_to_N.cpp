a26a3c513c4d70df09cbb79e8876905836110d66
Arihant416 <arihantjain416@gmail.com>
2022-08-12 22:23:59 +0530

    Recursion1 - 1ToN

---
+++ recursion/print_1_to_N.cpp
@@ -0,0 +1,43 @@
+/*
+Author : Arihant Jain
+github:https://github.com/Arihant416
+linkedin : https://www.linkedin.com/in/arihant416
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ull unsigned ll
+#define PB push_back
+#define MP make_pair
+#define flash ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define vi vector<int>
+#define vl vector<ll>
+#define vll vector<ull>
+#define mpi map<int, int>
+#define mpl map<ll, ll>
+#define mpll map<ull, ull>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define all(a) begin(a), end(a)
+#define maxEl(a) *max_element(all(a))
+#define minEl(a) *min_element(all(a))
+#define uimap unordered_map<int, int>
+#define ulmap unordered_map<ll, ll>
+#define mppii map<pi, int>
+
+void print1ToN(int i, int N)
+{
+  if (i == N + 1)
+    return;
+  cout << i << " ";
+  print1ToN(i + 1, N);
+}
+
+int32_t main()
+{
+  flash;
+  int N;
+  cin >> N;
+  print1ToN(1, N);
+  return 0;
+}
