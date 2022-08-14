6e0f6393ccb56236d996b06cae85f3780ddcb0ff
Arihant416 <arihantjain416@gmail.com>
2022-08-14 19:35:59 +0530

    Frequency of each element in array

---
+++ hashing/count_frequency.cpp
@@ -0,0 +1,55 @@
+/*
+Author : Arihant Jain
+github:https://github.com/Arihant416
+linkedin : https://www.linkedin.com/in/arihant416
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ull unsigned ll
+#define PB push_back
+#define MP make_pair
+#define flash ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define vi vector<int>
+#define vl vector<ll>
+#define vll vector<ull>
+#define mpi map<int, int>
+#define mpl map<ll, ll>
+#define mpll map<ull, ull>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define all(a) begin(a), end(a)
+#define maxEl(a) *max_element(all(a))
+#define minEl(a) *min_element(all(a))
+#define uimap unordered_map<int, int>
+#define ulmap unordered_map<ll, ll>
+#define mppii map<pi, int>
+
+uimap findFrequency(vi &A)
+{
+  uimap hash;
+  for (auto &i : A)
+    hash[i]++;
+  return hash;
+}
+
+void printMap(uimap &hash)
+{
+  for (auto &i : hash)
+  {
+    cout << i.first << " occurs " << i.second << " times." << endl;
+  }
+}
+
+int32_t main()
+{
+  flash;
+  int N;
+  cin >> N;
+  vi A(N);
+  for (auto &i : A)
+    cin >> i;
+  auto res = findFrequency(A);
+  printMap(res);
+  return 0;
+}
